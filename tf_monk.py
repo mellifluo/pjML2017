a36d630d9b369fbe6fd6c0719f73c8a527411e9c
mellifluo <alefagnani@yahoo.it>
2017-11-30 02:14:21 +0100

    modello fatto, risultati strani

--- tf_monk.py
+++ tf_monk.py
@@ -3,45 +3,55 @@
 import tensorflow
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 # load dataset
 X = np.loadtxt('./monk/monks-1.train', dtype='string', delimiter=' ')
 # label data
-y = X[:,1].astype(int)
+y = X[:,1].astype('float32')
 y = y[..., np.newaxis]
 # input data
-X = X[:,2:-1].astype(int)
-X.shape[0]
+X = X[:,2:-1].astype('float32')
 # settings
 inputlayer_neurons = int(X.shape[1])
 hiddenlayer_neurons = 3
 output_neurons = 1
 epoch = 100
 lr = 0.2
-# input
-a_0 = tf.placeholder(np.float32, X.shape)
-# output
-output = tf.placeholder(np.float32, y.shape)
-# weight and bias initialization
-# hidden layer
-w_1 = tf.Variable(tf.truncated_normal([inputlayer_neurons, hiddenlayer_neurons]))
-b_1 = tf.Variable(tf.truncated_normal([1, hiddenlayer_neurons]))
-# output layer
-w_2 = tf.Variable(tf.truncated_normal([hiddenlayer_neurons, output_neurons]))
-b_2 = tf.Variable(tf.truncated_normal([1, output_neurons]))
-# putting together
-z_1 = tf.add(tf.matmul(a_0, w_1), b_1)
-a_1 = tf.nn.sigmoid(z_1)
-z_2 = tf.add(tf.matmul(a_1, w_2), b_2)
-a_2 = tf.nn.sigmoid(z_2)
-# error and backprop
-diff = tf.subtract(a_2, y)
-cost = tf.multiply(diff, diff)
-step = tf.train.GradientDescentOptimizer(lr).minimize(cost)
+beta = 0.01
 
-sess = tf.InteractiveSession()
-sess.run(tf.global_variables_initializer())
+graph = tf.Graph()
+with graph.as_default():
+    # input
+    a_0 = tf.constant(X)
+    # output
+    output = tf.constant(y)
+    # weight and bias initialization
+    # hidden layer
+    w_1 = tf.Variable(tf.random_uniform([inputlayer_neurons, hiddenlayer_neurons], minval=0.1 , maxval=0.9 , dtype=tf.float32))
+    b_1 = tf.Variable(tf.random_uniform([1, hiddenlayer_neurons], minval=0.1 , maxval=0.9 , dtype=tf.float32))
+    # output layer
+    w_2 = tf.Variable(tf.random_uniform([hiddenlayer_neurons, output_neurons], minval=0.1 , maxval=0.9 , dtype=tf.float32))
+    b_2 = tf.Variable(tf.random_uniform([1, output_neurons], minval=0.1 , maxval=0.9 , dtype=tf.float32))
+    # putting together
+    a_1 = tf.nn.sigmoid(tf.add(tf.matmul(a_0, w_1), b_1))
+    a_2 = tf.nn.sigmoid(tf.add(tf.matmul(a_1, w_2), b_2))
+    # error and backprop
+    loss = tf.subtract(output, a_2)
+    step = tf.train.GradientDescentOptimizer(lr).minimize(loss)
 
-for i in range(epoch):
-    # print('before step {}, y is {}'.format(i, sess.run(output)))
-    sess.run(step, feed_dict = {a_0: X, output : y})
+with tf.Session(graph=graph) as sess:
+    sess.run(tf.global_variables_initializer())
+    # useful for visualization
+    writer = tf.summary.FileWriter("output", sess.graph)
+    # training
+    for i in range(epoch):
+        _ = sess.run(step)
+    writer.close()
+    # ancora non ho capito bene qui, error da risultati strani
+    # ammà chiedere a federico
+    error = sess.run(loss)
+    result = a_2.eval()
+
+plt.plot(y)
+plt.plot(error)
